67ac005813835a827e650830865e45d58c641ba3
dragons6641 <43373202+dragons6641@users.noreply.github.com>
2021-06-21 22:14:43 +0900

    Triangle Judgement
    https://leetcode.com/problems/triangle-judgement/

---
+++ SQL/leetcode610.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+SELECT x, y, z, 
+CASE WHEN x + y <= z THEN 'No'
+     WHEN y + z <= x THEN 'No'
+     WHEN z + x <= y THEN 'No'
+     ELSE 'Yes' END AS triangle
+FROM   triangle;
